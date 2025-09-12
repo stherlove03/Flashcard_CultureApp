edc9e77ee4517ba7c8e7734785acdef8972786c4
pris <prisnie11@gmail.com>
2025-09-12 18:40:35 -0400

    Adding the required and optional tasks for the Lab 2

--- app/src/main/java/com/example/cultureapp/AddCardActivity.kt
+++ app/src/main/java/com/example/cultureapp/AddCardActivity.kt
@@ -1,6 +1,11 @@
 package com.example.cultureapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +16,62 @@ class AddCardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val cancel_button=findViewById<ImageView>(R.id.cancel_button)
+        val save_button = findViewById<ImageView>(R.id.save_button)
+        val editing_question= intent.getStringExtra("added_question");
+        val editing_answer= intent.getStringExtra("added_answer");
+        val editing_wrong_answer= intent.getStringExtra("added_wrong_answer");
+        val editing_wrong_answer1= intent.getStringExtra("added_wrong_answer1");
+
+        findViewById<EditText>(R.id.add_question).setText(editing_question)
+        findViewById<EditText>(R.id.add_answer).setText(editing_answer)
+        findViewById<EditText>(R.id.add_wrong_answer).setText(editing_wrong_answer)
+        findViewById<EditText>(R.id.add_wrong_answer1).setText(editing_wrong_answer1)
+
+        cancel_button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            finish()
         }
+        val toast = Toast.makeText(applicationContext, "Must enter both the Question and Answer", Toast.LENGTH_SHORT)
+        //toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+
+        save_button.setOnClickListener {
+            val add_question = findViewById<EditText>(R.id.add_question).text
+            val add_answer = findViewById<EditText>(R.id.add_answer).text
+            val add_wrong_answer = findViewById<EditText>(R.id.add_wrong_answer).text
+            val add_wrong_answer1=findViewById<EditText>(R.id.add_wrong_answer1).text
+
+            if (add_question.isBlank() || add_answer.isBlank() || add_wrong_answer.isBlank() || add_wrong_answer1.isBlank() ) {
+                toast.show()
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                val data = Intent()
+
+                data.putExtra(
+                    "added_question",
+                    findViewById<EditText>(R.id.add_question).text.toString()
+                )
+
+                data.putExtra(
+                    "added_answer",
+                    findViewById<EditText>(R.id.add_answer).text.toString()
+                )
+                data.putExtra(
+                    "added_wrong_answer",
+                    findViewById<EditText>(R.id.add_wrong_answer).text.toString()
+                )
+                data.putExtra(
+                    "added_wrong_answer1",
+                    findViewById<EditText>(R.id.add_wrong_answer1).text.toString()
+                )
+
+                setResult(RESULT_OK, data) // set result code and bundle data for response
+
+                finish()
+            }
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/cultureapp/MainActivity.kt
+++ app/src/main/java/com/example/cultureapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.cultureapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.graphics.Color
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +22,8 @@ class MainActivity : AppCompatActivity() {
         val flashcardAnswer2 = findViewById<TextView>(R.id.flashcard_answer2)
         val toggleIcon = findViewById<ImageView>(R.id.toggleIcon)
         var isShowingAnswers = true
-
+        val add_button= findViewById<ImageView>(R.id.add_button)
+        val edit_button= findViewById<ImageView>(R.id.edit_button)
         // Quand on clique sur la question, elle disparaît et la réponse apparaît
 /*        flashcardQuestion.setOnClickListener {
             flashcardQuestion.visibility = View.INVISIBLE
@@ -53,6 +58,38 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data: Intent? = result.data
+            if (data != null) { // Check that we have data returned
+                val added_question = data.getStringExtra("added_question") // 'string1' needs to match the key we used when we put the string in the Intent
+                val added_answer= data.getStringExtra("added_answer")
+                val added_wrong_answer = data.getStringExtra("added_wrong_answer")
+                val added_wrong_answer1 = data.getStringExtra("added_wrong_answer1")
+
+                flashcardQuestion.text = added_question
+                flashcardAnswer2.text=added_answer
+                flashcardAnswer.text=added_wrong_answer
+                flashcardAnswer1.text=added_wrong_answer1
+            } else {
+                Log.i("MainActivity", "Returned null data from AddCardActivity")
+            }
+            Snackbar.make(findViewById(R.id.flashcard_question),
+                "Card Successfully Created",
+                Snackbar.LENGTH_SHORT)
+                .show()
+        }
+        add_button.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+        edit_button.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            intent.putExtra("added_question", flashcardQuestion.text.toString());
+            intent.putExtra("added_answer", flashcardAnswer2.text.toString());
+            intent.putExtra("added_wrong_answer", flashcardAnswer.text.toString());
+            intent.putExtra("added_wrong_answer1", flashcardAnswer1.text.toString());
+            resultLauncher.launch(intent)
+        }
     }
 }
 
